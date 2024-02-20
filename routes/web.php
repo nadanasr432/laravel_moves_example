2c5bf0e0145cbb04c70e5558f07a2adc2a5fbc57
nadanasr432 <nadanm432@gmail.com>
2024-02-20 19:11:16 +0200

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\ActorsController;
+use App\Http\Controllers\TvController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',  [MoviesController::class,'index'])->name('movies.index');
+Route::get('/movies/{id}', [MoviesController::class,'show'])->name('movies.show');
+Route::get('/actors', [ActorsController::class,'index'])->name('actors.index');
+Route::get('/actors/page/{page?}', [ActorsController::class,'index']);
+
+Route::get('/actors/{id}', [ActorsController::class,'show'])->name('actors.show');
+Route::get('/tv',  [TvController::class,'index'])->name('tv.index');
+Route::get('/tv/{id}', [TvController::class,'show'])->name('tv.show');
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,8 +14,57 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+         
+            $response->assertSuccessful();
+        
+        Http::fake([
+            'https://api.themoviedb.org/3/movie/popular'=>$this->fakePopularMovies(),
+            'https://api.themoviedb.org/3/movie/now_playing'=>$this->fakeNowPlayingMovies(),
+            'https://api.themoviedb.org/3/movie/genre/movie/list'=>$this->fakeGenres(),
 
-        $response->assertStatus(200);
+
+        ]);
+        $response = $this->get(route('movies.index'));
+
+        $response->assertSee('Popular Movies');
+        $response->assertSuccessful('Fake Movies');
+        $response->assertSuccessful();
+        $response->assertSee('Now Playing');
+        $response->assertSuccessful('Now Playing Fake Movies');
+        $response->assertSuccessful();
     }
-}
+    private function fakePopularMovies(){
+        return Http::response([
+            [
+                [
+                    "adult" => false,
+                    "backdrop_path" => "/jZIYaISP3GBSrVOPfrp98AMa8Ng.jpg",
+                    "genre_ids" =>  [
+                      0 => 16,
+                      1 => 35,
+                      2 => 10751,
+                      3 => 14,
+                      4 => 10749,
+                    ],
+                    "id" => 976573,
+                    "original_language" => "en",
+                    "original_title" => "Elemental",
+                    "overview" => "In a city where fire, water, land and air residents live together, a fiery young woman and a go-with-the-flow guy will discover something elemental: how much they have in common."      
+                   , "popularity" => 2075.582,
+                    "poster_path" => "/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg",
+                    "release_date" => "2023-06-14",
+                    "title" => "Fake Movies",
+                    "video" => false,
+                    "vote_average" => 7.8,
+                    "vote_count" => 1582,
+                ]]] ,200);
+
+    }
+    private function fakeNowPlayingMovies(){
+
+    }
+    private function fakeGenres(){
+
+    }
+
+    }
